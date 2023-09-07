8fc2a89b8d2f8285722317b71642d314f2105535
lhaj-matich <choumouloussama@gmail.com>
2023-09-08 00:17:40 +0100

    Finished implementing the user avatar update.

--- pong-server/src/users/users.service.ts
+++ pong-server/src/users/users.service.ts
@@ -34,7 +34,7 @@ export class UsersService {
   }
 
   findAll() {
-    return this.prismaService.user.findMany();
+    return this.prismaService.user.findMany;
   }
 
   updateUser(id: string, updateUserDto: UpdateUserDto) {
