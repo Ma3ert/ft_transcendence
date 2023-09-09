c7bdff455bea708aff7ae7da2fd496b1cbae3834
Ma3ert <yahyaitiazza15@gmail.com>
2023-09-09 12:53:12 +0100

    now the ball is moving and scale rightfully

--- frontend/app/Game/Ball.tsx
+++ frontend/app/Game/Ball.tsx
@@ -1,34 +1,51 @@
 import React from 'react'
 import { Box } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
+import { Decimal } from 'decimal.js';
 
 type Props = {}
 
 const Ball = (props: Props) => {
+	var box = 100;
 	const [bx, setBx] = useState(500)
 	const [by, setBy] = useState(900)
-	const [bw, setBw] = useState(100)
-	const [bh, setBh] = useState(100)
 	const [scale, setScale] = useState(by / 800)
-	useEffect(() => {
-		window.addEventListener('keydown', handleKeyDown);
-		return () => {
-		window.removeEventListener('keydown', handleKeyDown);
-		};
-	}, [by, bx]);
+	var boxSize = scale * box
 	const handleKeyDown = (event: KeyboardEvent) => {
 		if (event.key === "ArrowUp")
 		{
-			if (by - 10 >= 10)
+			if (by - 10 > 10)
 				setBy(by - 10)
 			setScale(by / 800)
+			boxSize = scale * box
 		}
 		else if (event.key === "ArrowDown"){
 			if (by + 10 < 1000)
 				setBy(by + 10)
 			setScale(by / 800)
+			boxSize = scale * box
+		}
+		else if (event.key === "ArrowRight")
+		{
+			if (bx + 10 < 1000)
+				setBx(bx + 10)
+		}
+		else if (event.key === "ArrowLeft")
+		{
+			if (bx - 10 > 10)
+				setBx(bx - 10)
 		}
 	}
+	useEffect(() => {
+		window.addEventListener('keydown', handleKeyDown);
+		// console.log("box size: " + boxSize)
+		// console.log("scale: " + scale.toString())
+		// console.log("y cordination: " + by.toString())
+		// console.log("------------------------------")
+		return () => {
+		window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [by, bx, box, scale]);
   	return (
 		<>
 			<Box
@@ -52,8 +69,7 @@ const Ball = (props: Props) => {
 				position={"absolute"}
 				top={by}
 				left={bx}
-				w={bw * scale}
-				h={bh * scale}
+				boxSize={boxSize.toString() + "px"}
 				borderRadius={"full"}
 				>
 			</Box>
