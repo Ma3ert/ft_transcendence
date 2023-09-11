a4ad2bb34ea896c7aa932cfa357ae02ba837ee7c
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-09-11 12:37:07 +0100

    add: chat module

--- pong-server/src/app.module.ts
+++ pong-server/src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { MulterModule } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { memoryStorage } from 'multer';
     MulterModule.register({ storage: memoryStorage() }),
     AuthModule,
     PassportModule.register({ session: true }),
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ pong-server/src/chat/chat.gateway.ts
@@ -0,0 +1,23 @@
+import { OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {Server, Socket} from 'socket.io'
+
+@WebSocketGateway({
+  origin:"*",
+  namespace:"chat"
+})
+
+export class ChatGateway implements OnGatewayConnection{
+
+  @WebSocketServer()
+  server : Server;
+
+  handleConnection(client:Socket) {
+    console.log(client.id);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(Socket: Socket) {
+
+  }
+  
+}
