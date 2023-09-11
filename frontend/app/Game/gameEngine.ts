1475d3c893dfde6d9b5ac2e713612555814f3f7e
Ma3ert <yahyaitiazza15@gmail.com>
2023-09-11 12:16:17 +0100

    the ball trajectory is done

--- frontend/app/Game/gameEngine.ts
+++ frontend/app/Game/gameEngine.ts
@@ -3,30 +3,70 @@ export interface Point {
 	y: number;
 }
 
-function getNextStep(current:Point, target:Point, stepSize:number) {
-	const dx: number = target.x - current.x;
-	const dy: number = target.y - current.y;
-	const angle: number = Math.atan2(dy, dx);
+// function getNextStep(current:Point, target:Point, stepSize:number) {
+// 	const dx: number = target.x - current.x;
+// 	const dy: number = target.y - current.y;
+// 	const angle: number = Math.atan2(dy, dx);
 
-	const xStep: number = stepSize * Math.cos(angle);
-	const yStep: number = stepSize * Math.sin(angle);
+// 	const xStep: number = stepSize * Math.cos(angle);
+// 	const yStep: number = stepSize * Math.sin(angle);
 
-	const newX: number = current.x + xStep;
-	const newY: number = current.y + yStep;
+// 	const newX: number = current.x + xStep;
+// 	const newY: number = current.y + yStep;
 
-	return { x: newX, y: newY };
-}
+// 	return { x: newX, y: newY };
+// }
 
-export function getBallTrajectory(start: Point, end: Point): Point[]
-{
-	var toAdd:Point = start;
-	var toReturn:Point[] = [];
-	while (toAdd.x !== end.x && toAdd.y !== end.y)
-	{
-		toAdd = getNextStep(toAdd, {x: 500, y:500}, 10);
-		toReturn.push(toAdd);
-	}
-	return (toReturn);
+// export function getBallTrajectory(start: Point, end: Point): Point[]
+// {
+// 	var toAdd:Point = start;
+// 	var toReturn:Point[] = [];
+// 	while (toAdd.x !== end.x && toAdd.y !== end.y)
+// 	{
+// 		toAdd = getNextStep(toAdd, {x: 500, y:500}, 10);
+// 		toReturn.push(toAdd);
+// 	}
+// 	return (toReturn);
+// }
+
+export function getBallTrajectory(start: Point, end: Point, step: number): Point[] {
+    const points: Point[] = [];
+    const dx = Math.abs(end.x - start.x);
+    const dy = Math.abs(end.y - start.y);
+    const sx = start.x < end.x ? 1 : -1;
+    const sy = start.y < end.y ? 1 : -1;
+    let err = dx - dy;
+	var count = step;
+
+    let x = start.x;
+    let y = start.y;
+
+    while (true) {
+		count -= 1;
+		if (count === 0)
+		{
+        	points.push({ x, y });
+			count = step;
+		}
+
+        if (x === end.x && y === end.y) {
+            break;
+        }
+
+        const e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y += sy;
+        }
+    }
+
+    return points;
 }
 
 export function getBallPositions(bottomLeft: Point, topLeft: Point, baseLine: number, topLine: number): Point[]
--- frontend/app/Game/page.tsx
+++ frontend/app/Game/page.tsx
@@ -8,19 +8,32 @@ type Props = {}
 
 export default function Home() {
   const positions: Point[] = getBallPositions({x: 270, y: 803}, {x: 460, y: 339}, 900, 400);
-  const trajectory: Point[] = getBallTrajectory(positions[2], positions[5])
+  const trajectory: Point[] = getBallTrajectory(positions[0], positions[9], 40)
   console.log(trajectory)
   return (
-    positions.map((point) => (
-      <Box
+    <>
+      {positions.map((point) => (
+        <Box
+          bg={"#fff"}
+          position={"absolute"}
+          boxSize={"10px"}
+          borderRadius={"full"}
+          top={point.y}
+          left={point.x}
+          >
+        </Box>
+      ))}
+      {trajectory.map((point) => (
+        <Box
         bg={"#fff"}
-        position={"absolute"}
-        boxSize={"10px"}
+        pos={"absolute"}
+        boxSize={"5px"}
         borderRadius={"full"}
         top={point.y}
         left={point.x}
-      >
-      </Box>
-    ))
+        >
+        </Box>
+      ))}
+    </>
   )
 }
