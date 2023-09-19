04a7bf2ae58b4c10613d2f4031f707e0e3f8f256
lhaj-matich <choumouloussama@gmail.com>
2023-09-19 21:23:52 +0100

    Solved migration issue.

--- pong-server/prisma/migrations/20230918050633_/migration.sql
+++ pong-server/prisma/migrations/20230918050633_/migration.sql
@@ -13,21 +13,3 @@ DROP TABLE "Game";
 
 -- DropTable
 DROP TABLE "Matchmaking";
-
--- CreateTable
-CREATE TABLE "ChannelMute" (
-    "userId" TEXT NOT NULL,
-    "channelId" TEXT NOT NULL,
-    "startMute" TIMESTAMP(3) NOT NULL,
-    "mutePeriod" INTEGER NOT NULL,
-    "isMuted" BOOLEAN NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "ChannelMute_pkey" PRIMARY KEY ("userId","channelId")
-);
-
--- AddForeignKey
-ALTER TABLE "ChannelMute" ADD CONSTRAINT "ChannelMute_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "ChannelMute" ADD CONSTRAINT "ChannelMute_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE CASCADE ON UPDATE CASCADE;
