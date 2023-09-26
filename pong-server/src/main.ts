69d9ca7c2b358659605084a48e9c03327573a017
lhaj-matich <choumouloussama@gmail.com>
2023-09-26 17:51:57 +0100

    Fixed the cors problem.

--- pong-server/src/main.ts
+++ pong-server/src/main.ts
@@ -7,9 +7,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['*'],
-  });
+  app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.use(passport.initialize());
