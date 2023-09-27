4fe35a286363e7b9d7c1f3462e83a8881a2f552e
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-09-27 14:52:07 +0100

    update: the bootstrap function to anableCors

--- pong-server/src/main.ts
+++ pong-server/src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({origin: ['http://localhost:3001'], credentials: true, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'});
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.use(passport.initialize());
