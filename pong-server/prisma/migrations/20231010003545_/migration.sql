2ea192054fee24ea201713edb2a7f7cb1e9fa4d4
ma3ert <yahyaitiazza15@gmail.com>
2023-10-13 10:05:38 +0100

    the same as before

--- pong-server/prisma/migrations/20231010003545_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[owner]` on the table `friendsList` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "friendsList_owner_key" ON "friendsList"("owner");
--- pong-server/prisma/migrations/20231012101209_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "blockedUsers_owner_key";
-
--- DropIndex
-DROP INDEX "userFriends_owner_key";
