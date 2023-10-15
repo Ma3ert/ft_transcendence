a44230c0c25596600d49d02914e80f101612062d
yassir <elatmaniyassir3@gmail.com>
2023-10-15 17:11:22 +0100

    Adding Envite to game message timer

--- frontend/src/components/ChatComponents/EnviteMessage.tsx
+++ frontend/src/components/ChatComponents/EnviteMessage.tsx
@@ -1,20 +1,20 @@
-import React, { useContext , useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { ChatContext, UsersContext } from "@/context/Contexts";
-import { HStack, Stack, Text, Image, Button } from "@chakra-ui/react";
-import {loggedIndUser} from "../../../contstants";
+import { HStack, Stack, Text, Image, Button, Progress } from "@chakra-ui/react";
+import { loggedIndUser } from "../../../contstants";
+import TimerComponent from "./TimerComponent";
 interface EnviteMessageProps {
   gameInviteSender?: string;
 }
 const EnviteMessage: React.FC<EnviteMessageProps> = ({}) => {
+  const { joinGameStatus, setJoinGameStatus, gameInviteSender } =
+    useContext(ChatContext);
+  const { activePeer, loggedInUser } = useContext(UsersContext);
 
-  const { joinGameStatus, setJoinGameStatus, gameInviteSender} = useContext(ChatContext);
-  const {activePeer, loggedInUser} = useContext(UsersContext) 
-
-
-   useEffect(() => {
+  useEffect(() => {
     // Create a setTimeout to change the message after 3 seconds
     const timerId = setTimeout(() => {
-      setJoinGameStatus! (false);
+      setJoinGameStatus!(false);
     }, 15000); // 3000 milliseconds (3 seconds)
 
     // Clean up the timer when the component unmounts or when needed
@@ -22,14 +22,14 @@ const EnviteMessage: React.FC<EnviteMessageProps> = ({}) => {
   }, []);
   return (
     <HStack
-      justify={(gameInviteSender != loggedIndUser.id)? "end" : "start"}
+      justify={gameInviteSender !== loggedIndUser.id ? "end" : "start"}
       w="98%"
       mx="auto"
       spacing={3}
     >
-      <HStack
+      <Stack
         borderRadius={"2xl"}
-        bg={(gameInviteSender != loggedIndUser.id) ? "#252932" : "#5B6171"}
+        bg={gameInviteSender !== loggedIndUser.id ? "#252932" : "#5B6171"}
         minW={"300px"}
         maxW="100%"
         px={2}
@@ -38,22 +38,47 @@ const EnviteMessage: React.FC<EnviteMessageProps> = ({}) => {
         h="auto"
         spacing={5}
       >
-        <Image src={(gameInviteSender != loggedIndUser.id) ?  "/LightSolidLogo.png" : "/DarkSolidLogo.png" } alt={"envite"} w={8} h={"auto"} />
-        <Stack justify={"center"} alignItems={"center"} p={2}>
-          <Text
-            color={(gameInviteSender != loggedIndUser.id) ? "#5B6171" : "#1D222C"}
-            fontSize={"sm"}
-            fontWeight={"bold"}
-          >
-            {(gameInviteSender != loggedIndUser.id) ? activePeer!.username :'you'} Looking for a 1v1 ..
-          </Text>
-          <Button onClick={()=>{
-            setJoinGameStatus! (false)
-          }} variant={(gameInviteSender != loggedIndUser.id) ? 'lightGray' : 'darkGray'} >
-            {`let's go`}
-          </Button>
-        </Stack>
-      </HStack>
+        <HStack spacing={5}>
+          <Image
+            src={
+              gameInviteSender !== loggedIndUser.id
+                ? "/LightSolidLogo.png"
+                : "/DarkSolidLogo.png"
+            }
+            alt={"envite"}
+            w={8}
+            h={"auto"}
+          />
+          <Stack justify={"center"} alignItems={"center"} p={2}>
+            <Text
+              color={
+                gameInviteSender !== loggedIndUser.id ? "#5B6171" : "#1D222C"
+              }
+              fontSize={"sm"}
+              fontWeight={"bold"}
+            >
+              {gameInviteSender !== loggedIndUser.id
+                ? activePeer!.username
+                : "you"}{" "}
+              Looking for a 1v1 ..
+            </Text>
+            <Button
+              isDisabled={gameInviteSender === loggedIndUser.id}
+              onClick={() => {
+                /// accept game invite
+                /// decline game inviate
+                setJoinGameStatus!(false);
+              }}
+              variant={
+                gameInviteSender !== loggedIndUser.id ? "lightGray" : "darkGray"
+              }
+            >
+              {`let's go`}
+            </Button>
+          </Stack>
+        </HStack>
+      <TimerComponent bg={gameInviteSender !== loggedIndUser.id ? "#5B6171" : "#1D222C"}/>
+      </Stack>
     </HStack>
   );
 };
--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -12,12 +12,17 @@ const config: ThemeConfig = {
 const ProgressStyle: ComponentStyleConfig = {
   variants: {
     default: {
-      colorScheme: "#252932",
+      colorScheme: "green",
       bg: "#5B6171",
       borderRadius: "full",
       p: "1px",
     },
+      custom: {
+          bg: "purple.500", // Set the background color of the bar
+          track: { bg: "gray.100" }, // Set the background color of the track
+        },
   },
+
 };
 
 const ModalStyle: ComponentStyleConfig = {
@@ -498,6 +503,12 @@ const InputStyle: ComponentStyleConfig = {
   },
 };
 
+const brand = {
+  100: "#DC585B",
+  200: "#DC585B",
+  500: "#DC585B", 
+  900: "#DC585B"
+}
 const theme = extendTheme({
   breakpoints: {
     // Define your custom breakpoint here
@@ -514,12 +525,14 @@ const theme = extendTheme({
     },
   },
   colors: {
+    brand,
     primaryDark: "#252932",
     secondDark: "#DC585B",
     thirdDark: "#F5F5F5",
     forthDark: "#5B6171",
   },
   components: {
+   
     Input: InputStyle,
     Button: ButtonStyle,
     Tabs: TabsStyle,
@@ -530,6 +543,7 @@ const theme = extendTheme({
   fonts: {
     visbyRound: "visbyRound, sans-serif",
   },
+  
 });
 
 export default theme;
