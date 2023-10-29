6496c0294f7312a30efcbcf248ebbac58ffe2a12
yassir58 <elatmaniyassir3@gmail.com>
2023-10-29 12:16:33 +0100

    Fixing channel members list

--- frontend/src/components/ChatComponents/Memberfield.tsx
+++ frontend/src/components/ChatComponents/Memberfield.tsx
@@ -2,23 +2,50 @@
 import React, {useContext, useState, useEffect} from 'react';
 import UserField from '../UserField';
 import { UsersContext } from '@/context/Contexts';
-import { Text } from '@chakra-ui/react';
+import { Button, HStack, Stack, Text } from '@chakra-ui/react';
+import UserAvatar from '../UserAvatar';
+import UserFieldNav from './UserFieldNav';
+import { useAuth } from '@/hooks/useAuth';
+import OptionsMenu from './FriendSettingsMenu';
 
 interface MemberFieldProps {
     member: Member
+    user:User
     members: Member[]
 }
-const MemberField:React.FC<MemberFieldProps> = ({member, members}) => {
+const MemberField:React.FC<MemberFieldProps> = ({member, members, user}) => {
 
-    const [user, setUser] = useState<User | null>(null);
     const {Users} = useContext(UsersContext)
+
+    const {currentUser} = useAuth ()
     useEffect(() => {
-        const user = Users!.find((user) => user.id === member.user);
-        if(user) {
-            setUser(user);
-        }
+       
     }, [Users, user])
-    return (user ? <UserField  member={member!} user={user!}  MembersList={members} /> : <Text>user is not found</Text>);
+    return (<Button variant={'field'} w={'100%'} h='auto'>
+        <HStack w='100%' h='100%' justify='space-between' alignItems='center'>
+            <HStack spacing={5} w='100%' justify='start' alignItems='center' px={3} py={2}>
+              <UserAvatar user={user!} />
+              <Stack spacing={4} w='100%' h='100%' justify='start' alignItems='start'>
+                <Text color='#D9D9D9' fontSize='sm'>{user!.username}</Text>
+                <Text color='#5B6171' fontSize='xs'>{member.role.toLowerCase ()}</Text>
+              </Stack>
+            </HStack>
+            
+            <UserFieldNav
+              member={member!}
+              user={user!}
+            />
+
+          {user!.id != currentUser.user!.id && (
+            <OptionsMenu
+              member={member!}
+              user={user!}
+              MembersList={members!}
+            />
+          )}
+        </HStack>
+            
+    </Button>);
 }
 
 export default MemberField;
